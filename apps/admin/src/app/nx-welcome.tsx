a44879ba051b5b42f229a519de3bdbc24fb0aa50
Ken <kf9173@gmail.com>
2024-08-30 12:41:38 -0400

    test3

--- apps/admin/src/app/nx-welcome.tsx
+++ apps/admin/src/app/nx-welcome.tsx
@@ -418,7 +418,7 @@ export function NxWelcome({ title }: { title: string }) {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there, </span>
+              <span> Hello there ---, </span>
               Welcome {title} 👋
             </h1>
           </div>
